94748c1906715fb8d37ba123a17d103d08eb6b1e
ammaridrees210 <link2ammar@gmail.com>
2025-01-17 12:51:54 +0500

    Fetch moves from Fixture and add more scenarios

--- cypress/e2e/checker.test.js
+++ cypress/e2e/checker.test.js
@@ -1,18 +1,41 @@
-import {checker} from "../support/pages/checkerGame5Moves/CheckerGamePO";
-import { game } from "../support/pages/CompletecheckerGame/checkerGamePO";
-const landingPage = new checker()
-const checkerGame = new game()
+import {checkerInitial5Steps} from "../support/pages/checkerGame5Moves/CheckerGamePO";
+import { checkerGamePlay } from "../support/pages/CompletecheckerGame/checkerGamePO";
+import { negativeCases } from "../support/pages/checkerGameNeagtiveCases/negativeCasesPO"
+const checkerCases = new negativeCases
+const intialGameSteps = new checkerInitial5Steps()
+const checkerGame = new checkerGamePlay()
 describe("CheckMate Game", () => {
     beforeEach(() => {
+        // Visit the CheckerGame page
         cy.visit("/");
     });
     
-    it("should visit the CheckerGame Page and Play 5 Move", () => {
-        // Assertion
-        landingPage.checker5StepsGame();
+    it("User should visit the CheckerGame and Play 5 Move", () => {
+        // POM "CheckerGamePO"
+        intialGameSteps.checker5StepsGame();
+    });
+        //negative testing Senarios 
+    it('Should redirect to the correct Checkers game URL',() => {
+        checkerCases.ensureCheckerGameUrl();
+    })    
+    it('User should allow a simple move for a Piece', () => {
+         // POM "negativeCasesPO.js"
+        checkerCases.ensureForwardMoves();
     });
 
-    it.only("Checker Game should be completed and Win", () => {
+    it('User should not allow a piece to move Backward', () => {
+         // POM "negativeCasesPO.js"
+        checkerCases.ensureReverseMoves();
+    })
+
+    it('User should not allow a piece to move on Opponent Piece', () => {
+         // POM "negativeCasesPO.js"
+        checkerCases.ensureMoveOnOpponentPiece();
+    })
+
+    it("User should visit, play complete Checker Game, and Win", () => {
+        // POM "checkerGamePO.js"
         checkerGame.checkGamePlayMoves();
     });
+
 });
--- cypress/support/pages/CompletecheckerGame/checkerGamePO.js
+++ cypress/support/pages/CompletecheckerGame/checkerGamePO.js
@@ -1,72 +1,33 @@
-export class game{
+export class checkerGamePlay{
     checkerGamePageVerify(){
         cy.get('.page').should('contain','Checkers')
     }
     clickOnRestartButton(){
         return cy.get('a').contains('Restart...')
     }
-    pieceToMoveSelector(position){
+    pieceToMove(position){
         return cy.get(`[name="space${position}"]`)
     }
-    positionToMovePieceSelector(position){
+    positionToMovePiece(position){
         return cy.get(`[name="space${position}"]`)
     }
     VerifyWonMassage(){
         return cy.get('#message').should('contain','You have won!')
     }
-    checkGamePlayMoves(){
-        this.checkerGamePageVerify()
-        this.clickOnRestartButton().click()
-        this.pieceToMoveSelector('02').click()
-        this.positionToMovePieceSelector('13').click().wait(2000)
-        this.pieceToMoveSelector('11').click()
-        this.positionToMovePieceSelector('02').click().wait(2000)
-        this.pieceToMoveSelector('00').click()
-        this.positionToMovePieceSelector('11').click().wait(2000)
-        this.pieceToMoveSelector('42').click()
-        this.positionToMovePieceSelector('24').click().wait(2000)
-        this.pieceToMoveSelector('31').click()
-        this.positionToMovePieceSelector('42').click().wait(2000)
-        this.pieceToMoveSelector('42').click()
-        this.positionToMovePieceSelector('53').click().wait(2000)
-        this.pieceToMoveSelector('22').click()
-        this.positionToMovePieceSelector('44').click().wait(2000)
-        this.pieceToMoveSelector('24').click()
-        this.positionToMovePieceSelector('35').click().wait(2000)
-        this.pieceToMoveSelector('13').click()
-        this.positionToMovePieceSelector('35').click().wait(2000)
-        this.pieceToMoveSelector('11').click()
-        this.positionToMovePieceSelector('22').click().wait(2000)
-        this.pieceToMoveSelector('20').click()
-        this.positionToMovePieceSelector('11').click().wait(2000)
-        this.pieceToMoveSelector('40').click()
-        this.positionToMovePieceSelector('31').click().wait(2000)
-        this.pieceToMoveSelector('22').click()
-        this.positionToMovePieceSelector('13').click().wait(2000)
-        this.pieceToMoveSelector('13').click()
-        this.positionToMovePieceSelector('04').click().wait(2000)
-        this.pieceToMoveSelector('35').click()
-        this.positionToMovePieceSelector('26').click().wait(2000)
-        this.pieceToMoveSelector('26').click()
-        this.positionToMovePieceSelector('17').click().wait(2000)
-        this.pieceToMoveSelector('04').click()
-        this.positionToMovePieceSelector('26').click().wait(2000)
-        this.pieceToMoveSelector('31').click()
-        this.positionToMovePieceSelector('42').click().wait(2000)
-        this.pieceToMoveSelector('36').click()
-        this.positionToMovePieceSelector('37').click().wait(2000)
-        this.pieceToMoveSelector('37').click()
-        this.positionToMovePieceSelector('46').click().wait(2000)
-        this.pieceToMoveSelector('26').click()
-        this.positionToMovePieceSelector('37').click().wait(2000)
-        this.pieceToMoveSelector('37').click()
-        this.positionToMovePieceSelector('46').click().wait(2000)
-        this.pieceToMoveSelector('02').click()
-        this.positionToMovePieceSelector('24').click().wait(2000)
-        this.pieceToMoveSelector('02').click()
-        this.positionToMovePieceSelector('24').click().wait(2000)
-        this.pieceToMoveSelector('11').click()
-        this.positionToMovePieceSelector('33').click().wait(2000)
-        this.VerifyWonMassage()
+
+    checkGamePlayMoves() {
+        cy.fixture('moves').then((data) => {
+            // Verify the checker game page and restart the game
+            this.checkerGamePageVerify();
+            this.clickOnRestartButton().click();
+            // Iterate through the moves and perform the actions
+            data.moves.forEach(({ piece, position }) => {
+                this.pieceToMove(piece).click();
+                this.positionToMovePiece(position).click().wait(2000);
+            });
+
+            // Verify the winning message
+            this.VerifyWonMassage();
+        });
     }
 }
